dcd6928aee1e2792798ad9e88ad93f8c83833be7
Indrani Koley <indranikoley66@gmail.com>
2024-09-29 00:53:34 +0530

    quiz-app

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Who is the Father of our Nation?",
+        option1: "RabindraBath Tagore",
+        option2: "Mahatma Gandhi",
+        option3: "Narendra Modi",
+        option4: "Dr. B. R. Ambedkar",
+        ans: 2
+    },
+    {
+        question: "Which is the most sensitive organ in our body?",
+        option1: "Skin",
+        option2: "Leg",
+        option3: "Nose",
+        option4: "Hair",
+        ans: 1
+    },
+    {
+        question: "Who was the first Prime Minister of India?",
+        option1: "Jawaharlal Nehru",
+        option2: "Subhas Chandra Bose",
+        option3: "Narendra Modi",
+        option4: "Dr. B. R. Ambedkar",
+        ans: 1
+    },
+    {
+        question: "Who invented Computer?",
+        option1: "XYZ",
+        option2: "Charles Babbage",
+        option3: "Darwins",
+        option4: "Rasbihari",
+        ans: 2
+    },
+    {
+        question: "Smallest state of India is?",
+        option1: "Tripura",
+        option2: "Kerala",
+        option3: "Goa",
+        option4: "Punjab",
+        ans: 3
+    },
+]
